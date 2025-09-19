83563bc93a127e650da849a0e64ed79318f542cb
carmenguerrav <carmenguerravence@icloud.com>
2025-09-19 14:02:44 +0200

    Add files via upload

---
+++ Query_TikTokDB.sql
@@ -0,0 +1,27 @@
+-- Parte 4
+
+-- Query 1
+SELECT * FROM usuarios;
+
+-- Query 2
+SELECT * FROM videos;
+
+-- Query 3
+SELECT * FROM comentarios;
+
+-- Query 4
+SELECT * FROM likes;
+
+-- Query 5 
+SELECT * FROM seguidores;
+
+-- Query 6 
+SELECT pais FROM usuarios
+WHERE pais = 'Argentina'; 
+
+-- Query 7 
+SELECT correo FROM usuarios; 
+
+-- Query 8 
+SELECT fecha_registro FROM usuarios; 
+
